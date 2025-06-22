bbd79f650e4d77461af8b11c598cb3d1324fe413
adrianatmelcop <adrianatheilmc@gmail.com>
2025-06-22 12:28:08 -0300

    arquivo de povoamento

---
+++ Povoamento.sql
@@ -0,0 +1,24 @@
+-- POVOAMENTO --
+
+/*
+Tabelas:
+
+Pessoa
+Endereco
+Telefone
+Email
+Cliente
+Funcionario
+Cargo
+Produto
+Genero_produto
+Produtora_produto
+Criadores_produto
+Bonus
+Conta
+Ganha
+Avalia
+Avalia2
+Aluga
+
+*/
