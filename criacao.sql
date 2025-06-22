d16de70abc3e5e2768993a2d041b5ca9992ae393
adrianatmelcop <adrianatheilmc@gmail.com>
2025-06-22 12:22:49 -0300

    mudanças nos atributos de Pessoa

--- criacao.sql
+++ criacao.sql
@@ -2,19 +2,19 @@
 
 --PESSOA
 CREATE TABLE Pessoa (
-	cpf VARCHAR2(12) NOT NULL,
-	nome VARCHAR2(25) NOT NULL,
-    genero VARCHAR2(10) NOT NULL, 
+	cpf VARCHAR2(14) NOT NULL,
+	nome VARCHAR2(50) NOT NULL,
+    genero CHAR(1) NOT NULL, 
 	nascimento DATE NOT NULL,
-    cep VARCHAR2(8) NOT NULL,
-    num_endereço VARCHAR2(4) NOT NULL,
+    cep VARCHAR2(9) NOT NULL,
+    num_endereço VARCHAR2(5) NOT NULL,
 	CONSTRAINT pessoa_pkey PRIMARY KEY (cpf),
     CONSTRAINT cep_fkey FOREIGN KEY (cep, num_endereço) REFERENCES Endereço(cep, num_endereço)
 );
 
 CREATE TABLE Endereco(
-    cep VARCHAR2(8) NOT NULL,
-    num_endereço VARCHAR2(4) NOT NULL,
+    cep VARCHAR2(9) NOT NULL,
+    num_endereço VARCHAR2(5) NOT NULL,
     complemento VARCHAR2(30),
     rua VARCHAR2(40) NOT NULL,
     bairro VARCHAR2(15)NOT NULL,
@@ -23,14 +23,14 @@ CREATE TABLE Endereco(
 );
 
 CREATE TABLE Telefone(
-    cpf_t VARCHAR2(12) NOT NULL,
+    cpf_t VARCHAR2(14) NOT NULL,
     telefone VARCHAR2(11) NOT NULL,
     CONSTRAINT telefone_pkey PRIMARY KEY (telefone, cpf_t),
     CONSTRAINT telefone_fkey FOREIGN KEY (cpf_t) REFERENCES Pessoa(cpf),
 );
 
 CREATE TABLE Email(
-    cpf_e VARCHAR2(12) NOT NULL,
+    cpf_e VARCHAR2(14) NOT NULL,
     email VARCHAR2(30) NOT NULL,
     CONSTRAINT email_pkey PRIMARY KEY (cpf_e, email),
     CONSTRAINT email_fkey FOREIGN KEY (cpf_e) REFERENCES Cliente(cpf_c),
@@ -38,16 +38,16 @@ CREATE TABLE Email(
 
 --CLIENTES
 CREATE TABLE Cliente( 
-    cpf_c VARCHAR2(12) NOT NULL,
-    cpf_indicador VARCHAR2(12) NOT NULL,
+    cpf_c VARCHAR2(14) NOT NULL,
+    cpf_indicador VARCHAR2(14) NOT NULL,
     CONSTRAINT cliente_pkey PRIMARY KEY (cpf_c),
     CONSTRAINT cliente_fkey FOREIGN KEY(cpf_c, cpf_indicador) REFERENCES Cliente(cpf_c)
 );
 
 CREATE TABLE Dependete(
-    cpf_responsavel VARCHAR2(12) NOT NULL,
+    cpf_responsavel VARCHAR2(14) NOT NULL,
     num_seq INTEGER NOT NULL,
-    nome VARCHAR2(10) NOT NULL,
+    nome VARCHAR2(50) NOT NULL,
     CONSTRAINT dependente_pkey  PRIMARY KEY(cpf_responsavel, num_seq),
     CONSTRAINT dependente_fkey FOREIGN KEY (cpf_responsavel) REFERENCES Cliente(cpf_c)
 );
@@ -58,9 +58,9 @@ CREATE SEQUENCE num_alugueis INCREMENT BY 1 START WITH 0;
 
 CREATE TABLE Funcionario(
     num_alugueis INTEGER,
-    cpf_f VARCHAR2(12) NOT NULL,
+    cpf_f VARCHAR2(14) NOT NULL,
     cargo VARCHAR2(13) NOT NULL,
-    cpf_s VARCHAR2(12) NOT NULL,
+    cpf_s VARCHAR2(14) NOT NULL,
     CONSTRAINT funcionario_pkey PRIMARY KEY (cpf_f),
     CONSTRAINT funcionario_fkey FOREIGN KEY (cpf_f) REFERENCES Pessoa(cpf),
     CONSTRAINT funcionario_fkey2 FOREIGN KEY (cpf_s) REFERENCES Funcionário(cpf_f),
@@ -121,11 +121,11 @@ CREATE SEQUENCE num INCREMENT BY 1 START WITH 1;
 
 CREATE TABLE Conta(
     num INTEGER NOT NULL,
-    cpf_cc VARCHAR2(12) NOT NULL,
+    cpf_cc VARCHAR2(14) NOT NULL,
     ---criacao DATE NOT NULL,
     credito NUMBER,
     qnt_alugada INTEGER,
-    CONSTRAINT conta_pkey PRIMARY KEY (num,cpf_cc)
+    CONSTRAINT conta_pkey PRIMARY KEY (num,cpf_cc),
     CONSTRAINT conta_fkey FOREIGN KEY (cpf_cc) REFERENCES Cliente(cpf_c),
 );
 
